6dd3f4d480cbd31421f53e299c1043dbfe10fc41
AnteroPaivarinta <75854430+AnteroPaivarinta@users.noreply.github.com>
2022-04-22 16:38:41 +0300

    Update server.js

--- javascript/server.js
+++ javascript/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const fs = require('fs')
-let notes = [{name: "joo", price: "30.30", link: "xwkfpowe"}];
+let notes = [];
 
 
 
